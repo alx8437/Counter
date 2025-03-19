d818fbc5be02fc0e64c84bcb7ee628a6db44ebd5
Aleks Zelenskiy <dezion777@gmail.com>
2025-03-19 13:06:11 +0300

    Set new values

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,21 @@ import {Settings} from "./Settings";
 function App() {
     const [startValue, setStartValue] = useState<number>(0);
     const [endValue, setEndValue] = useState<number>(5);
+    const [userValue, setUserValue] = useState<number>(startValue);
 
     const setNewValues = (minValue: number, maxValue: number) => {
         setStartValue(minValue)
+        setUserValue(minValue)
         setEndValue(maxValue)
+    }
+
+    const setValue = (newValue: number) => {
+        setUserValue(newValue);
     }
 
     return (
         <div className="container">
-            <Counter startValue={startValue} endValue={endValue}/>
+            <Counter setValue={setValue} startValue={startValue} endValue={endValue} userValue={userValue}/>
             <Settings setNewValues={setNewValues} startValue={startValue} endValue={endValue} />
         </div>
     );
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,15 +1,15 @@
-import React, {FC, useState} from 'react';
+import React, {FC} from 'react';
 
 type CounterPropsType = {
     startValue: number
     endValue: number
+    userValue: number
+    setValue: (newValue: number) => void
 }
 
-export const Counter:FC<CounterPropsType> = ({startValue, endValue}) => {
-    const [value, setValue] = useState<number>(startValue);
-
+export const Counter:FC<CounterPropsType> = ({startValue, endValue, userValue, setValue}) => {
     const increment = () => {
-        const newValue = value + 1
+        const newValue = userValue + 1
         setValue(newValue);
     }
 
@@ -17,14 +17,14 @@ export const Counter:FC<CounterPropsType> = ({startValue, endValue}) => {
         setValue(startValue)
     }
 
-    const inputClassName = `counter__input ${value === endValue ? 'error' : ''}`
+    const inputClassName = `counter__input ${userValue === endValue ? 'error' : ''}`
 
     return (
         <div className='counter'>
-            <input value={value} className={inputClassName} type="text"/>
+            <input value={userValue} className={inputClassName} type="text"/>
             <div>
-                <button disabled={value === endValue} onClick={increment} className='counter__button'>inc</button>
-                <button disabled={value === startValue} onClick={resetInitialValue} className='counter__button'>reset</button>
+                <button disabled={userValue === endValue} onClick={increment} className='counter__button'>inc</button>
+                <button disabled={userValue === startValue} onClick={resetInitialValue} className='counter__button'>reset</button>
             </div>
         </div>
     );
